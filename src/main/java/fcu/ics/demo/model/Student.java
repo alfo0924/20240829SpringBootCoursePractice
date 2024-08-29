d05ebcefa2eb43a3b2f6bc3db1135b135e013e42
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-29 12:02:09 +0800

    v1.022 add Student.java
    init cnp

--- src/main/java/fcu/ics/demo/model/Student.java
+++ src/main/java/fcu/ics/demo/model/Student.java
@@ -1,12 +1,18 @@
 package fcu.ics.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private int studentId;
     private String firstName;
     private String lastName;
     private String email;
     private String birthday;
+
+
 }
